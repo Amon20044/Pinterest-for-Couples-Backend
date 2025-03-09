b915c0ca00ab2d7f09189331b43469918bd439e0
Amon Sharma <159574376+AmonSharma2004@users.noreply.github.com>
2025-03-09 15:43:46 +0530

    remove

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { registerUser,loginUser, getUserBy_ID } = require('../controllers/userController');
-const router = express.Router();
-
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.get('/:id', getUserBy_ID);
-
-module.exports = router;
